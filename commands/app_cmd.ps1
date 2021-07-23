629babbbed088943e4efaf8376ebd1183df80d95
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2021-07-23 10:13:30 +0200

    Removed commands folder

--- commands/app_cmd.ps1
+++
@@ -1,7 +0,0 @@
-﻿cd C:\Users\jean.demeusy\Desktop\data-logger\
-.\venv\Scripts\activate
-
-
-Start http://localhost:5000/activites
-
-python .\src\app.py
--- commands/wood_plug_cmd.ps1
+++
@@ -1,6 +0,0 @@
-﻿cd C:\Users\jean.demeusy\Desktop\data-logger\
-.\venv\Scripts\activate
-
-python .\src\event_main.py -a111527
-
-deactivate -and exit
